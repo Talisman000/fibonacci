1018dcacf6995acaa1fe6de7f6e7410c03c5c5a9
Talisman000 <s.k.recode226@gmail.com>
2020-09-18 22:45:59 +0900

    fixed

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+const memory = new Map();
+memory.set(0, 0);
+memory.set(1, 1);
+function fib(n) {
+  if (memory.has(n)) {
+    return memory.get(n);
+  }
+  const value = fib(n - 1) + fib(n - 2);
+  memory.set(n, value);
+  return value;
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
